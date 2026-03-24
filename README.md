dfe4bf1f606ccf059dd230f5259bd2867faf7abc
geooooo <geo97it@gmail.com>
2026-03-25 00:24:43 +0300

    implement styles and prepare basic ui

---
+++ README.md
@@ -0,0 +1,3 @@
+# WAP - Web Audio Player by Rust
+
+The perfect reimplemetation of [old project](https://github.com/geooooo/wap-web_audio_player).
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use leptos::prelude::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button
+            on:click=move |_| set_count.set(count.get() + 1_i32)
+        >
+            "Click me: " {move || count.get()}
+        </button>
+        <p>
+            "count: " {count}
+        </p>
+    }
+}
+
+fn main() {
+    console_error_panic_hook::set_once();
+
+    leptos::mount::mount_to_body(App);
+}
